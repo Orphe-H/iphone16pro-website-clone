62d2b850bd74356926f9d3b8eabf734fd17d634c
Bobby Orphé HOUESSINON <orphe.h00@gmail.com>
2025-02-18 10:34:15 +0100

    update dependencies and rf

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,24 @@
-import { hydrate, prerender as ssr } from 'preact-iso';
+import { hydrate, prerender as ssr } from "preact-iso";
 
-import { Navbar } from './pages/home/sections/navbar';
-import { Content } from './pages/home/sections/content';
-import { Footer } from './pages/home/sections/footer';
+import { Navbar } from "./pages/home/sections/navbar";
+import { Content } from "./pages/home/sections/content";
+import { Footer } from "./pages/home/sections/footer";
 
-import './assets/js/app.js';
-import './assets/css/app.css';
+import "./assets/js/app.js";
+import "./assets/css/app.css";
 
 export function App() {
 	return (
 		<>
-			<Navbar/>
-			<Content/>
-			<Footer/>
+			<Navbar />
+			<Content />
+			<Footer />
 		</>
 	);
 }
 
-if (typeof window !== 'undefined') {
-	hydrate(<App />, document.getElementById('app'));
+if (typeof window !== "undefined") {
+	hydrate(<App />, document.getElementById("app"));
 }
 
 export async function prerender(data) {
